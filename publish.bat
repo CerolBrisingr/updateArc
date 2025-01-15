d39f72c20060b18dd6430fc8eade55a80f8782a3
Sebastian <bastianbaur@gmx.de>
2025-01-15 18:25:42 +0100

    Less verbose publish script

--- publish.bat
+++ publish.bat
@@ -10,11 +10,13 @@ set EXEPATH=.\build\%PROJECT%\%BUILD%
 mkdir target
 mkdir target\source
 
-robocopy "." ".\target" LICENSE
-robocopy ".\%PROJECT%" ".\target\source" /s /mt
+robocopy "." ".\target" LICENSE > robocopy.txt
+robocopy ".\%PROJECT%" ".\target\source" /s /mt >> robocopy.txt
 
-robocopy ".\%PROJECT%\resources" ".\target" *.dll
-robocopy %EXEPATH% ".\target" %PROJECT%.exe
-robocopy "." ".\target" publish.bat
+robocopy ".\%PROJECT%\resources" ".\target" *.dll >> robocopy.txt
+robocopy %EXEPATH% ".\target" %PROJECT%.exe >> robocopy.txt
+robocopy "." ".\target" publish.bat >> robocopy.txt
+
+echo Moving of files to target directory complete. View robocopy.txt if any issues arise.
 
 exit 0
